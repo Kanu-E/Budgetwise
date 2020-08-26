2ace0cf1c4c7a2e147eaa299bacd3f54031e0c4e
kanu-e <chukwuemekakanu@gmail.com>
2020-08-26 17:02:40 -0500

    completed crud function

--- app/controllers/expense_controller.rb
+++ app/controllers/expense_controller.rb
@@ -14,12 +14,26 @@ class ExpenseController < ApplicationController
         erb :'expense/index'   
     end
 
+    get '/expenses/:id' do
+        @expense = Expense.find(params["id"])
+        erb :'expense/show'
+    end
+
     get '/expenses/:id/edit' do
         @expense = Expense.find(params[:id])
         erb :'expense/edit'
     end
 
+    patch '/expenses/:id' do
+        expense = Expense.find(params[:id])
+        expense.update(title: params[:title], amount: params[:amount])
+        redirect '/expenses'
+    end
 
-
+    delete '/expenses/:id' do
+        expense = Expense.find(params[:id])
+        expense.destroy
+        redirect '/expenses'
+    end
 
 end
