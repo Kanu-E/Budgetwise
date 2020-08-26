e76fd952438664b5de0be50285b5f878da697da4
kanu-e <chukwuemekakanu@gmail.com>
2020-08-26 12:51:09 -0500

    create expense show

--- app/controllers/expense_controller.rb
+++ app/controllers/expense_controller.rb
@@ -1,11 +1,17 @@
 class ExpenseController < ApplicationController 
   
-    get '/expense/new' do
+    get '/expenses/new' do
         erb :'expense/new'   
     end
 
     post '/expenses' do
       exp =  Expense.create(params)
+      redirect '/expenses'
+    end
+
+    get '/expenses' do
+        @expenses = Expense.all
+        erb :'expense/show'   
     end
 
 end
