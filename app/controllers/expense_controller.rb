4a19d34ad681151c896c5c8fdbb71215aefca6fb
kanu-e <chukwuemekakanu@gmail.com>
2020-08-28 20:07:43 -0500

    ensuring users can't edit others content

--- app/controllers/expense_controller.rb
+++ app/controllers/expense_controller.rb
@@ -1,16 +1,18 @@
 class ExpenseController < ApplicationController 
   
     get '/expenses/new' do
-        erb :'expense/new'   
+        if logged_in?
+            erb :'expense/new'   
+        end
     end
 
     post '/expenses' do
       @exp =  current_user.expenses.build(params)
-      if @exp.save
-      redirect '/expenses'
-      else
-        erb :'expense/new'   
-      end
+        if @exp.save
+            redirect '/expenses'
+        else
+            erb :'expense/new'   
+        end
     end
 
     get '/expenses' do
@@ -19,13 +21,19 @@ class ExpenseController < ApplicationController
         else  
             redirect '/signin'
         end
-
     end
 
     get '/expenses/:id/edit' do
-        @expense = Expense.find(params[:id])
-        if @expense.user = current_user
-        erb :'expense/edit'
+        if logged_in?
+            @expense = Expense.find(params[:id])
+        else
+            redirect '/signin'
+        end
+        
+        if @expense.user == current_user
+            erb :'expense/edit'
+        else
+            redirect '/expenses'
         end
     end
 
@@ -37,8 +45,12 @@ class ExpenseController < ApplicationController
 
     delete '/expenses/:id' do
         expense = Expense.find(params[:id])
+        if expense.user == currrent_user
         expense.destroy
         redirect '/expenses'
+        else
+        redirect '/expenses'
+        end
     end
-
+    
 end
